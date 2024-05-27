f0593b1f70ddf1a65e7591dd44bc2e415e194349
Deepakraja03 <12306deepakraja@gmail.com>
2024-05-27 09:28:38 +0530

    removed bcrypt

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
 require('dotenv').config();
 
 const app = express();
