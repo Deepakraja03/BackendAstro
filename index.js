8b6e319af6ff4f261af2f0d91bc32fe6749bfcb4
Deepakraja03 <12306deepakraja@gmail.com>
2024-05-27 09:37:12 +0530

    added login and register

--- index.js
+++ index.js
@@ -56,6 +56,42 @@ const userSchema = new mongoose.Schema({
   const Slot = mongoose.model('Slot', slotSchema);
   
 
+  app.post('/api/register', async (req, res) => {
+    const { admin, password } = req.body;
+    try {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const newUser = new User({
+        admin,
+        password: hashedPassword
+      });
+      await newUser.save();
+      res.status(201).json({ message: 'User registered successfully' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Server error' });
+    }
+  });
+  
+  app.post('/api/login', async (req, res) => {
+    const { admin, password } = req.body;
+    try {
+      const user = await User.findOne({admin});
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+      const isPasswordValid = bcrypt.compare(password, user.password);
+      if (!isPasswordValid) {
+        return res.status(401).json({ message: 'Invalid password' });
+      }
+  
+      res.status(200).json({ message: 'Login successful' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Server error' });
+    }
+  });
+  
+
   app.post('/api/slots', async (req, res) => {
     const { date, starttime, endtime, mode } = req.body;
   
