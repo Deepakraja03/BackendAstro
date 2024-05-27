da6bdc1a7c3fbf800ba3efb38ba5944ec1e9f691
Deepakraja03 <12306deepakraja@gmail.com>
2024-05-27 09:25:02 +0530

    login and register

--- index.js
+++ index.js
@@ -44,8 +44,8 @@ const userSchema = new mongoose.Schema({
   
   const slotSchema = new mongoose.Schema({
     date: Date,
-    starttime: String,  // Ensure field name matches
-    endtime: String,    // Ensure field name matches
+    starttime: String,
+    endtime: String,
     mode: String,
     isBooked: {
       type: Boolean,
@@ -53,8 +53,45 @@ const userSchema = new mongoose.Schema({
     }
   });
   
-  const Slot = mongoose.model('Slot', slotSchema);
+const Slot = mongoose.model('Slot', slotSchema);
 
+
+app.post('/api/register', async (req, res) => {
+    const { admin, password } = req.body;
+    try {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const newUser = new User({
+        admin,
+        password: hashedPassword
+      });
+      await newUser.save();
+      res.status(201).json({ message: 'User registered successfully' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Server error' });
+    }
+  });
+  
+  app.post('/api/login', async (req, res) => {
+    const { admin, password } = req.body;
+    try {
+      const user = await User.findOne({admin});
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+      const isPasswordValid = bcrypt.compare(password, user.password);
+      if (!isPasswordValid) {
+        return res.status(401).json({ message: 'Invalid password' });
+      }
+  
+      res.status(200).json({ message: 'Login successful' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Server error' });
+    }
+  });
+
+  
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
